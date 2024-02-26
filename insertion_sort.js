38b184ca1f49db5870765800a85780935472c29d
Gold <aoie8086@gmail.com>
2024-02-26 15:25:41 +0900

    insertion

---
+++ insertion_sort.js
@@ -0,0 +1,30 @@
+const IS = class IS {
+  constructor(target, mode) {
+    this.target = target;
+    this.mode = mode != null ? mode : 'ascending'; // descending
+  }
+  random() {
+    this.target = [];
+    const num = Math.random(0, 10) * 10;
+    for (let g = 0; g < num; ++ g)
+      this.target.push(Math.random(0, 10) * 10);
+  }
+  boot() {
+    let e = 1;
+    while (e < this.target.length) {
+      for (let g = e; g > 0; -- g) {
+        if (this.target[g] >= this.target[g - 1]) break;
+        const tmp = this.target[g - 1];
+        this.target[g - 1] = this.target[g];
+        this.target[g] = tmp;
+      }
+      e += 1;
+    }
+    return this.target;
+  }
+}
+
+const is = new IS();
+is.random();
+console.log(is.target);
+console.log(is.boot());
